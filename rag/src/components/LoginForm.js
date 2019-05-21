8bf50a28dc83e95a54df7dbf4c181cd2a81f120e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-21 09:26:02 -0500

    RAG:Added actions, reducers for setting the Login authentication
    Added actions, reducers, store, Links and PrivateRoute for setting up the login and authentication

--- rag/src/components/LoginForm.js
+++ rag/src/components/LoginForm.js
@@ -3,36 +3,107 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 import BeTheChange from "../assets/rag_pic_change.png";
 import "../styles/login.css";
+import { login } from "../actions";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-function LoginForm(props) {
-  return (
-    <div>
-      <Nav isLoggedIn={false} />
-      <div className="body-container">
-        <section className="left-section">
-          <img
-            className="left-image-text"
-            src={BeTheChange}
-            alt="Be The Change"
-          />
-        </section>
-        <section className="right-section">
-          <div className="right-section-container-login">
-            <div className="form-text">Email</div>
-            <div className="form-element">
-              <input className="form-input" type="text" />
+class LoginForm extends React.Component {
+  state = {
+    credentials: {
+      email: "",
+      password: ""
+    }
+  };
+  componentDidMount = () => {
+    if (this.props.location.state) {
+      console.log(this.props.location.state);
+      if (
+        this.props.location.state.email &&
+        this.props.location.state.password
+      ) {
+        this.setState({
+          credentials: {
+            email: this.props.location.state.email,
+            password: this.props.location.state.password
+          }
+        });
+      }
+    }
+  };
+  handleChanges = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  onLogin = e => {
+    e.preventDefault();
+    this.props.login(this.state.credentials).then(() => {
+      this.props.history.push("/homepage");
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Nav isLoggedIn={false} />
+        <div className="body-container">
+          <section className="left-section">
+            <img
+              className="left-image-text"
+              src={BeTheChange}
+              alt="Be The Change"
+            />
+          </section>
+          <section className="right-section">
+            <p>
+              Sign in below to access your "justhelp" page. Don't have an
+              account yet?{" "}
+              <Link className="link-register" to="/">
+                Create one here.
+              </Link>
+            </p>
+
+            <div className="right-section-container-login">
+              <form onSubmit={this.onLogin} autoComplete="false">
+                <div className="form-text">Email</div>
+                <div className="form-element">
+                  <input
+                    className="form-input"
+                    type="text"
+                    name="email"
+                    placeholder="Email Address"
+                    value={this.state.credentials.email}
+                    onChange={this.handleChanges}
+                  />
+                </div>
+                <div className="form-text">Password</div>
+                <div className="form-element">
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.credentials.password}
+                    onChange={this.handleChanges}
+                  />
+                  <button className="login-btn" onClick={this.onLogin}>
+                    Start Changing
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="form-text">Password</div>
-            <div className="form-element">
-              <input className="form-input" type="password" />
-              <button className="login-btn">Start Changing</button>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
+        <div className="login-filler" />
+        <Footer />
       </div>
-      <div className="login-filler" />
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
-export default LoginForm;
+
+export default connect(
+  null,
+  { login }
+)(LoginForm);
--- rag/src/index.js
+++ rag/src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
