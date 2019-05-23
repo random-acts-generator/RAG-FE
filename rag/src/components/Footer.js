1f18a0014015288955fb21df72b766dd79060ccb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-23 09:11:11 -0500

    RAG: Minor updates - links and buttons
    Added superficial updates to make all the links work

--- rag/src/components/Footer.js
+++ rag/src/components/Footer.js
@@ -5,7 +5,9 @@ function Footer(props) {
   return (
     <div className="footer-container">
       <div className="footer-left">
-        <div>About</div>
+        <div>
+          <a href="https://justhelp.netlify.com/about.html">About</a>
+        </div>
         <div>Contact Us</div>
       </div>
       <div className="footer-right">Powered by LambdaBW</div>
